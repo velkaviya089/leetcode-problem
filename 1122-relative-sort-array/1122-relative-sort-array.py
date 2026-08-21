8247dbf57da6316e47587bfaee0e7650c01058e4
velkaviya089 <velkaviya089@gmail.com>
2026-08-21 22:09:53 +0530

    Time: 0 ms (100%), Space: 19.2 MB (97.85%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        order={num: i for i,num in enumerate(arr2)}
+        arr1.sort(key=lambda x:(order.get(x,len(arr2)),x))
+        return arr1
+        
