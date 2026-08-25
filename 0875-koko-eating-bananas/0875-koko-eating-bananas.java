8bfccfaeb638fc8fba6ccc9ebafdd50f9f5b75d1
velkaviya089 <velkaviya089@gmail.com>
2026-08-25 10:24:19 +0530

    Time: 8 ms (67.57%), Space: 47.9 MB (44.43%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left=1;
+        int right=0;
+        for (int pile:piles){
+            right=Math.max(right,pile);
+        }
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            long hours=0;
+            for(int pile:piles){
+                hours+=(pile+mid-1)/mid;
+            }
+            if(hours<=h){
+                right=mid-1;
+
+            } else {
+                left=mid+1;
+                
+            }
+        }
+        return left;
+    }
+}
