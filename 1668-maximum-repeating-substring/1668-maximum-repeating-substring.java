78b409355437f2c5d778ac5ac3d89a59a648fb5a
velkaviya089 <velkaviya089@gmail.com>
2026-08-27 00:08:41 +0530

    Time: 1 ms (90.29%), Space: 43.2 MB (33.86%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int count=0;
+        String temp=word;
+        while(sequence.contains(temp)){
+            count++;
+            temp=temp+word;
+        }
+        return count;
+    }
+}
