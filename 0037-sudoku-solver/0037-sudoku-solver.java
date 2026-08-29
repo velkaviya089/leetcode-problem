600911cb8b5fdfd617b5906b6d31c6bc113215de
velkaviya089 <velkaviya089@gmail.com>
2026-08-29 23:47:58 +0530

    Time: 232 ms (55.99%), Space: 43 MB (40.4%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,49 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+    boolean backtrack(char[][] board){
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if (board[row][col]=='.'){
+                    for(char num='1';num<='9';num++){
+                        if(isValid(board,row,col,num)){
+                            board[row][col]=num;
+                            if(backtrack(board)){
+                                return true;
+                            }
+                            board[row][col]='.';
+                        }
+                    }        
+                    return false;
+                }        
+                        
+                    
+            }
+        }
+        return true;
+    }
+    boolean isValid(char[][]board,int row,int col,char num){
+        for (int j=0;j<9;j++){
+            if (board[row][j]==num){
+                return false;
+            }
+        }
+        for (int i=0;i<9;i++){
+            if (board[i][col]==num){
+                return false;
+            }
+        }
+
+        int startRow=(row/3)*3;
+        int startCol=(col/3)*3;
+        for (int i=startRow;i<startRow+3;i++){
+            for(int j=startCol;j<startCol+3;j++){
+                if(board[i][j]==num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
