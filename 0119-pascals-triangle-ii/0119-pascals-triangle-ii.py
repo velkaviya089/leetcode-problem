9dfc0847bb90c1218e305804228a9898ca627065
velkaviya089 <velkaviya089@gmail.com>
2026-08-30 10:30:48 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.59%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row=[1]
+        for i in range(1,rowIndex+1):
+            row.append(row[-1]*(rowIndex-i+1)//i)
+        return row
